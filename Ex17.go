bc5527422e5f61dd22b6bfbf77f6cf5c7300103b
Ketan Gaikwad <ketangaikwad6384@gmail.com>
2023-05-07 03:28:04 +0530

    Switch cases

---
+++ Ex17.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	favsport := "Cricket"
+
+	switch favsport {
+	case "favsport":
+		fmt.Println("Football is fav sport")
+
+	case "Cricket":
+		fmt.Println("Cricket is favsport")
+	}
+}
